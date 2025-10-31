859b49158095cd0cf44f5d4795602be3a1c83059
soumya.hiremath <01fe24ica013@kletech.ac.in>
2025-10-31 11:40:27 +0530

    calculate simple interest modified

--- simpleinterest.py
+++ simpleinterest.py
@@ -3,27 +3,28 @@ def calculate_simple_interrest(principal,rate,time):
     """Calculate simple interest given principal,rate,time."""
     return(principal*rate*time)/100
 
-def main():
-     print("=== Simple Interest Calculator===")
-     try:
-         if len(sys.argv)==4:
-             p=float(sys.argv[1])
-             r=float(sys.argv[2])
-             t=float(sys.argv[3])
-         else:
-             p=100000.0
-             r=5 
-             t=1  
-         print("\n=== Program Parameters===")
-         print(f"Principal Amount:{p}")
-         print(f"Rate of interest:{r}")
-         print(f"Time in years:{t}")   
+if__name__=="__main__":
+    print("=== Simple Interest Calculator ===")
+    try:
+        if len(sys.argv) == 4:
+            p = float(sys.argv[1])
+            r = float(sys.argv[2])
+            t = float(sys.argv[3])
+            
+        else:
+            p = float(input("Enter the principal amount: "))
+            r = float(input("Enter the rate of interest: "))
+            t = float(input("Enter the time in years: "))
+            
+         print("\n====Program Parameters====")
+         print("Principal Amount :",p)
+         print("Rate of interest :",r) 
+         print("Time in years :",t) 
          
-         interest=calculate_simple_interest(p,r,t)
-         print(f"\nSimple Interest={interest.2f}")
+         interest = calculate_simple_interest(p,r,t)
+         print(f"\nSimple interest={interest:.2f}")
          
-    except ValueError:
-        print("Invalid input! please enter numeric values.")
-        
-if __name__=="__main__":
-     main()             
+    except ValueError:   
+        print("Invalid input! Please enter numeric values.")  
+
+                
